71f9de8f975fef0f52262338149c866f0c117a74
Henry <hg097870pq@gmail.com>
2025-06-08 17:54:42 -0500

    A

--- animations/menuPlace.js
+++
@@ -1,4 +0,0 @@
-import { animate } from "animejs";
-
-// Asegúrate de que el menú tenga position: fixed o absolute y un left inicial fuera de pantalla
-animate('.nav-cont', { x:"100px", duration: 1000,ease:'outCubic' });
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import '../draggable/square.js'
-import '../javascript/subMenu.js'
-import '../javascript/menuHover.js'
-import '../animations/menuPlace.js'
-import '../animations/contentMain.js'
-import '../styles/actividades.css'
-import '../animations/cardsActivities.js'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
